512b1089349ad5f73aa00bd13585116a41519bde
Fene-87 <fenekayas87@gmail.com>
2022-08-05 05:25:31 -0900

    add product to inventory

--- src/components/funcComponents.js
+++ src/components/funcComponents.js
@@ -1,10 +1,31 @@
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import { useState } from 'react';
 
 export default function AddProduct(){
+  // typeOfData [stateData, stateUpdateFunction] = useState(initialData)
+  let initialValue = [];
+  const [products, setProduct] = useState(initialValue);
+
+  // method to handle product
+  const add = (event)=>{
+    event.preventDefault();
+    const formData = event.target;
+    const newProduct = {
+      product_name: formData.pdt_name.value,
+      price: formData.price.value,
+      quantity: formData.qty.value
+    };
+    
+    //add a new product inside products array
+    setProduct([...products,newProduct]);
+  }
+
+
     return(
         <div>
-          <Form>
+          <Form onSubmit = {add}>
             <Form.Group className="mb-3" controlId="formBasicProductName">
               <Form.Label>Product Name</Form.Label>
               <Form.Control type="text" placeholder="Enter Product Name" name="pdt_name"/>
@@ -27,6 +48,33 @@ export default function AddProduct(){
               Add to Inventory
             </Button>
           </Form>
+
+          <Table striped bordered hover variant="dark">
+           <thead>
+             <tr>
+               <th>index</th>
+               <th>Product Name</th>
+               <th>Price</th>
+               <th>Quantity</th>
+             </tr>
+           </thead>
+
+           <tbody>
+            {
+              products.map((item, index)=>{
+                return(
+                  <tr key = {index}>
+                 <td>{index}</td>
+                 <td>{item.product_name}</td>
+                 <td>{item.price}</td>
+                 <td>{item.quantity}</td>
+                </tr>
+                )
+              })
+            }
+            
+           </tbody>
+         </Table>
         </div>
     );
 }
